04e0ea8ff9770836de193f358b5ac2df9ca7dad8
Scott <enslin21@yahoo.com>
2024-02-13 11:12:37 -0500

    finish up project with styling and functionality

--- src/app/page-1/page-1.component.ts
+++ src/app/page-1/page-1.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { Category } from './category';
 
 @Component({
   selector: 'app-page-1',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag, FormsModule],
   templateUrl: './page-1.component.html',
   styleUrl: './page-1.component.scss'
 })
+
 export class Page1Component {
+  newCategory = '';
+  displayCreateNewCategory = true;
+  categories: Category[] = [];
+
+  drop(event: CdkDragDrop<Category[]>) {
+    moveItemInArray(this.categories, event.previousIndex, event.currentIndex);
+  }
+
+  removeCategory(category: Category) {
+    this.categories = this.categories.filter((currCat: Category) => currCat.name !== category.name );
+  }
+
+  saveCategory() {
+    this.categories.push({name: this.newCategory, seoNumber: this.categories.length + 1});
+    this.newCategory = '';
+    this.displayCreateNewCategory = false;
+  }
+
+  showNewCategorySection() {
+    this.displayCreateNewCategory = true;
+  }
 
 }
